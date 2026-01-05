78067776907dd6361239f009151dfaa5f41ba78b
Abushaker Jamil <abushakerjamil254@gmail.com>
2026-01-05 22:41:10 +0600

    add basic function

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -3,7 +3,80 @@ pragma solidity ^0.8.25;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract RebaseToken is ERC20 {
-    constructor() ERC20("STAKING", "STK") {}
-}
+/**
+ * @title RebaseToken
+ * @author Abushaker Jamil
+ * @notice This is a cross-chain rebase token that incentivises users to deposit into a vault and gain interest in rewards.
+ * @notice The interest rate in the smart contract can only decrease.
+ * @notice Each user will have their own interest rate that is the global interest rate at the time of deposit.
+ */
 
+contract RebaseToken is ERC20 {
+    error RebaseToken__InterestRateCanOnlyDecrease(uint256 oldinterestRate, uint256 newInterestRate);
+
+    uint256 private s_InterestRate = 5e10;
+    uint256 private constant PRECISION_FACTOR = 1e18;
+    mapping(address => uint256) s_userInterestRate;
+    mapping(address => uint256) s_userLastUpdatedTimestamp;
+
+    event InterestRateSet(uint256 newInterestRate);
+
+    constructor() ERC20("Rebase Token", "RBT") {}
+
+    function setInterestRate(uint256 _newInterestRate) external {
+        if (_newInterestRate < s_InterestRate) {
+            revert RebaseToken__InterestRateCanOnlyDecrease(s_InterestRate, _newInterestRate);
+        }
+        s_InterestRate = _newInterestRate;
+        emit InterestRateSet(_newInterestRate);
+    }
+
+    function mint(address _to, uint256 _amount) external {
+        _mintAccruedInterest(_to);
+        s_userInterestRate[_to] = s_InterestRate;
+        _mint(_to, _amount);
+    }
+
+    function balanceOf(address _user) public view override returns (uint256) {
+        // Get the user's stored principal balance (tokens actually minted to them).
+        return super.balanceOf(_user) * _calculateUserAccumulatedInterestSinceLastUpdate(_user) / PRECISION_FACTOR;
+    }
+
+    function _calculateUserAccumulatedInterestSinceLastUpdate(address _user)
+        internal
+        view
+        returns (uint256 linearInterestFactor)
+    {
+        // 1. Calculate the time elapsed since the user's balance was last effectively updated.
+        uint256 timeElapsed = block.timestamp - s_userLastUpdatedTimestamp[_user];
+
+        // If no time has passed, or if the user has no locked rate (e.g., never interacted),
+        // the growth factor is simply 1 (scaled by PRECISION_FACTOR).
+        if (timeElapsed == 0 || s_userInterestRate[_user] == 0) {
+            return PRECISION_FACTOR;
+        }
+
+        // 2. Calculate the total fractional interest accrued: UserInterestRate * TimeElapsed.
+        // s_userInterestRate[_user] is the rate per second.
+        // This product is already scaled appropriately if s_userInterestRate is stored scaled.
+        uint256 fractionalInterest = s_userInterestRate[_user] * timeElapsed;
+
+        // 3. The growth factor is (1 + fractional_interest_part).
+        // Since '1' is represented as PRECISION_FACTOR, and fractionalInterest is already scaled, we add them.
+        linearInterestFactor = PRECISION_FACTOR + fractionalInterest;
+        return linearInterestFactor;
+    }
+
+    function _mintAccruedInterest(address _user) internal {
+        // TODO: Implement full logic to calculate and mint actual interest tokens.
+        // The amount of interest to mint would be:
+        // current_dynamic_balance - current_stored_principal_balance
+        // Then, _mint(_user, interest_amount_to_mint);
+
+        s_userLastUpdatedTimestamp[_user] = block.timestamp;
+    }
+
+    function getUserInterestRate(address _user) external view returns (uint256) {
+        return s_userInterestRate(_user);
+    }
+}
